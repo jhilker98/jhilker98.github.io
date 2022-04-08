53d1f91b431aa629fddf117b256f783e01b48af7
Jacob Hilker <jacob.hilker2@gmail.com>
2022-04-08 13:39:06 -0400

    added content-org

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((eval . (setq-local
+                  org-roam-directory (expand-file-name "./content-org" default-directory)))
+         (eval .
+               (setq-local org-roam-db-location (expand-file-name "./.org-roam.db" default-directory))))))
