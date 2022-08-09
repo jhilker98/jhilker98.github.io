78d76b841972f1f7e9ce26bf782b8ef2f7e1c51b
Jacob Hilker <jacob.hilker2@gmail.com>
2022-08-09 11:36:57 -0400

    deleted dir-locals

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((eval . (setq-local
-                  org-roam-directory (expand-file-name "./content-org" default-directory)))
-         (eval .
-               (setq-local org-roam-db-location (expand-file-name "./.org-roam.db" default-directory))))))
