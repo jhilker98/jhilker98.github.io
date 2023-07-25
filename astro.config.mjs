22d512441e7c49f634d209fd013b8e913bfcc885
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-25 18:48:29 -0400

    added assets and drafts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +6,14 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind(), alpinejs()]
+  integrations: [mdx(), tailwind(), alpinejs()],
+  experimental: {
+    assets: true,
+  },
+  markdown: {
+    drafts: true,
+    shikiConfig: {
+      theme: "material-theme-darker",
+    },
+  }
 });
