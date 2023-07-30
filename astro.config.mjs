8a15499e9295d024754b57dcdb29dcaec2071f02
Jacob Hilker <jacob.hilker2@gmail.com>
2023-07-30 10:20:05 -0400

    enabled rehype heading id

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
+
+
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind(), alpinejs()],
@@ -12,6 +16,9 @@ export default defineConfig({
   },
   markdown: {
     drafts: true,
+    rehypePlugins: [
+      rehypeHeadingIds
+    ],
     shikiConfig: {
       theme: "material-theme-darker",
     },
