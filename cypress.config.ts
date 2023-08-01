ce7a5c780780308d5617ecd5ac6680566e79aa64
Jacob Hilker <jacob.hilker2@gmail.com>
2023-08-01 15:53:04 -0400

    converted to typescript

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
     e2e: {
-        supportFile: false
+        supportFile: false,
+        baseUrl: "http://localhost:3000"
     }
 })
--- cypress/e2e/index.cy.js
+++
@@ -1,6 +0,0 @@
-import { cy, it } from "cypress";
-it('titles are correct', () => {
-    const page = cy.visit('http://localhost:3000');
-    page.get('title').should('have.text', "Home - Jacob's Website"
-    )
-});
