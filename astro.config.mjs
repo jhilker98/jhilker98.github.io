9e9b61381a295bc6b9f17807f7204e2e9cbd6df0
Jacob Hilker <jacob.hilker2@gmail.com>
2023-10-07 19:47:24 -0400

    added sitemap, configured coverUrl

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,24 @@
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import remarkToc from 'remark-toc';
 
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jhilker.com",
   outDir: "public",
   publicDir: "static",
-  integrations: [mdx(), tailwind(), alpinejs()],
-
+  integrations: [mdx(), tailwind(), alpinejs(), sitemap()],
   markdown: {
     drafts: true,
-    rehypePlugins: [
-      rehypeHeadingIds,
-      remarkToc,
-    ],
+    rehypePlugins: [rehypeHeadingIds, remarkToc],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true
-    },
-  },
-
-
+    }
+  }
 });
