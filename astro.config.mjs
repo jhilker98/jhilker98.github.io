0883d85d219ad3c99ab845f4bd41f16e3150b87b
Jacob Hilker <jacob.hilker2@gmail.com>
2024-03-11 14:06:02 -0400

    re-installed astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,16 @@ import alpinejs from "@astrojs/alpinejs";
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import remarkToc from 'remark-toc';
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jhilker.com",
   outDir: "public",
   publicDir: "static",
-  integrations: [mdx(), tailwind(), alpinejs(), sitemap(), robotsTxt()],
+  integrations: [mdx(), tailwind(), alpinejs(), sitemap(), robotsTxt(), icon()],
   markdown: {
     rehypePlugins: [rehypeHeadingIds, remarkToc],
     shikiConfig: {
