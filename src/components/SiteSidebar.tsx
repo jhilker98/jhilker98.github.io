9429d1d8e766d3fdd7127001f3e589661d3897ca
Jacob Hilker <jacob.hilker2@gmail.com>
2025-10-12 15:28:34 -0400

    refactor: fadd sidebar dependencies

---
+++ src/components/SiteSidebar.tsx
@@ -0,0 +1,21 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import { type VariantProps, cva } from "class-variance-authority";
+
+export function SiteSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  )
+}
