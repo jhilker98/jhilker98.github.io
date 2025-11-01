246f7c69fb818a25b36dca6c4549bd3971191e2c
Jacob Hilker <jacob.hilker2@gmail.com>
2025-11-01 14:50:25 -0400

    refactor: reorder and enhance documentation for slugifyPostDate and related functions

--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -2,15 +2,11 @@ import type { BlogPost } from "@/content.config";
 import getReadingTime from "reading-time";
 
 /**
- * Filters a list of posts by the name of the series, sorted by the order of each post in that series.
- * @param {string} series The name of the series. Is slugified in the function to compare totals.
- * @param {blogPost[]} posts The list of blog posts to search through.
- * @return {*}  {BlogPost[]} The array of blog posts in the series, sorted by order in series.
+ * 
+ * @param {Date} d The publication date of the post.
+ * @returns {string} A slugified string in the format "YYYY/MM" representing the year and month of the date.
  */
-
-
-
-export function slugifyPostDate(d: Date) {
+export function slugifyPostDate(d: Date): string {
   const f = {
     year: new Intl.DateTimeFormat("en-US", { year: "numeric" }),
     month: new Intl.DateTimeFormat("en-US", { month: "2-digit" }),
@@ -19,6 +15,12 @@ export function slugifyPostDate(d: Date) {
   return `${f.year.format(d)}/${f.month.format(d)}`;
 }
 
+
+/**
+ * Takes in a string and returns a slugified version of it. 
+ * @param {string} text The URL to be slugified.
+ * @returns {string} The slugified version of the input string.
+ */
 export function slugifyUrl(text: string) {
   return text
     .toString()
@@ -29,6 +31,13 @@ export function slugifyUrl(text: string) {
     .replace(/^-+/, "")
     .replace(/-+$/, "");
 }
+
+/**
+ * Filters a list of posts by the name of the series, sorted by the order of each post in that series.
+ * @param {string} series The name of the series. Is slugified in the function to compare totals.
+ * @param {blogPost[]} posts The list of blog posts to search through.
+ * @return {*}  {BlogPost[]} The array of blog posts in the series, sorted by order in series.
+ */
 
 export function findPostsInSeries(
   series: string,
@@ -42,7 +51,13 @@ export function findPostsInSeries(
     (a: BlogPost, b: BlogPost) => a.data.series.weight - b.data.series.weight,
   );
 }
-export function formatPostDate(date: Date) {
+
+/**
+ * Formats a date into a human-readable string for blog posts.
+ * @param {Date} date The publication date of the post.
+ * @returns {string} A formatted date string in the format "D Mon. YYYY".
+ */
+export function formatPostDate(date: Date): string {
   const dateString = new Date(date)
     .toLocaleDateString("en-GB", {
       timeZone: "UTC",
@@ -55,6 +70,11 @@ export function formatPostDate(date: Date) {
   return `${dateString[0]} ${dateString[1]}. ${dateString[2]}`;
 }
 
+/**
+ * Creates an array of hashtags from an array of tags.
+ * @param {string[]} originalTags The list of tags from a given post.
+ * @returns {string[]} An array of hashtags created from the original tags.
+ */
 export function hashtagPostTags(originalTags: string[]) {
   const tags: string[] = [];
   for (const tag of originalTags) {
@@ -63,11 +83,21 @@ export function hashtagPostTags(originalTags: string[]) {
   return tags;
 }
 
+/**
+ * Calculates the estimated reading time for a given article.
+ * @param {string} article The article content as a string.
+ * @returns {string} The estimated reading time in a human-readable format.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function calcReadingTime(article: string): string {
   return getReadingTime(article.trim()).text;
 }
 
+/**
+ * converts a string to Title Case.
+ * @param {string} str The string to be converted to Title Case.
+ * @returns {string} The input string converted to Title Case.
+ */
 export function toTitleCase(str: string): string {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
